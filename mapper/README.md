1d995c972716a9e5d40c8c08e93a7a49dfb45d91
Anshuman <makemefriend.anshu@gmail.com>
2019-03-15 02:52:10 +0530

    intial logic
    intial logic

--- mapper/README.md
+++ mapper/README.md
@@ -1,21 +1,18 @@
 # Mapper
 
-**TODO: Add description**
-
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `mapper` to your list of dependencies in `mix.exs`:
+## TODO
 
 ```elixir
-def deps do
-  [
-    {:mapper, "~> 0.1.0"}
-  ]
-end
+Mapper.lookup(phonenumber)
+```
+
+## Usage
+
+```elixir
+Mapper.prepare_dictionary()
+Mapper.lookup
 ```
 
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/mapper](https://hexdocs.pm/mapper).
-
--- mapper/lib/mapper.ex
+++ mapper/lib/mapper.ex
@@ -1,11 +1,33 @@
 defmodule Mapper do
-
   def prepare_dictionary do
+    file = "dictionary/dictionary.txt"
 
+    if File.regular?(file) == false do
+      {:ok, content} = :zip.unzip(~c'dictionary/dictionary.zip', [{:cwd, ~c'dictionary/'}])
+      IO.inspect(content)
+    end
+
+    words =
+      File.stream!(file)
+      |> Stream.map(&String.trim_trailing/1)
+      |> Enum.to_list()
+      |> List.to_tuple()
+      |> IO.puts()
+
+    case File.read(file) do
+      {:ok, body} ->
+        String.split(body)
+        |> length
+        |> IO.puts()
+
+      {:error, reason} ->
+        "Could not read file #{file} because #{reason}"
+        |> IO.puts()
+    end
   end
 
-  def lookup(phone) do
-    prepare_dictionary()  
+  def lookup() do
+    prepare_dictionary()
     store = Mnemonix.new(fizz: 1)
     Mnemonix.get(store, :fizz)
   end
