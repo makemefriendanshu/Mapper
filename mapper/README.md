71195b86eb590fc943a485dab83fb13f504e5e45
Anshuman <makemefriend.anshu@gmail.com>
2019-03-24 06:32:32 +0530

    changes

--- mapper/README.md
+++ mapper/README.md
@@ -11,5 +11,31 @@ Mapper.lookup(phonenumber)
 ```elixir
 iex -S mix
 Mapper.prepare_dictionary()
+
 Mapper.lookup(2282668687)
+[["act", "amounts"], ["act", "contour"], ["acta", "mounts"], ["bat", "amounts"], ["bat", "contour"], ["cat", "contour"], "catamounts"]
+Mapper.lookup(6686787825)
+[["motor", "usual"], ["noun", "struck"], ["nouns", "truck"], ["nouns", "usual"], ["onto", "struck"], "motortruck"]
+
+{[
+   {["act", "bat", "cat"], ["amounts", "contour"]},
+   {"acta", "mounts"},
+   {nil, "mumus"},
+   {nil, ["tots", "tour"]},
+   {nil, ["mus", "nus", "our"]},
+   {nil, nil},
+   {nil, nil},
+   {"catamounts", nil}
+ ]}
+
+ {[
+   {["mot", "not", "oot"], nil},
+   {["noun", "onto"], "struck"},
+   {["motor", "nouns"], ["truck", "usual"]},
+   {nil, ["puck", "ruck", "suck"]},
+   {nil, "taj"},
+   {nil, nil},
+   {nil, nil},
+   {"motortruck", nil}
+ ]}
 ```
--- mapper/lib/mapper.ex
+++ mapper/lib/mapper.ex
@@ -43,33 +43,30 @@ defmodule Mapper do
 
     store = Mnemonix.new()
 
-    list =
-      File.stream!(file)
-      |> Stream.map(&String.trim_trailing(&1, "\n"))
-      |> Stream.filter(&(String.length(&1) < 11))
-      |> Stream.filter(&(String.length(&1) >= 3))
-      |> Stream.filter(&(String.length(&1) != 8))
-      |> Stream.filter(&(String.length(&1) != 9))
-      |> Stream.map(&String.downcase(&1))
-      |> Stream.map(&{&1, transform(&1)})
-      |> Enum.to_list()
-      |> Enum.map(fn {k, v} ->
-        in_store = Mnemonix.get(store, v)
+    File.stream!(file)
+    |> Stream.map(&String.trim_trailing(&1, "\n"))
+    |> Stream.filter(&(String.length(&1) < 11))
+    |> Stream.filter(&(String.length(&1) >= 3))
+    |> Stream.filter(&(String.length(&1) != 8))
+    |> Stream.filter(&(String.length(&1) != 9))
+    |> Stream.map(&String.downcase(&1))
+    |> Stream.map(&{&1, transform(&1)})
+    |> Enum.to_list()
+    |> Enum.map(fn {k, v} ->
+      in_store = Mnemonix.get(store, v)
 
-        if in_store == nil do
-          Mnemonix.put_new(store, v, k)
+      if in_store == nil do
+        Mnemonix.put_new(store, v, k)
+      else
+        if is_list(in_store) do
+          Mnemonix.put(store, v, in_store ++ [k])
         else
-          if is_list(in_store) do
-            Mnemonix.put(store, v, in_store ++ [k])
-          else
-            Mnemonix.put(store, v, [in_store] ++ [k])
-          end
+          Mnemonix.put(store, v, [in_store] ++ [k])
         end
-      end)
+      end
+    end)
 
     store
-
-    # Enum.reduce(list, %{}, fun [h|t] -> {h, &string_to_digit(h)})
   end
 
   def lookup(phone) do
@@ -78,41 +75,40 @@ defmodule Mapper do
 
     if(!(phone =~ "0" || phone =~ "1")) do
       {
-        terms =
-          2..9
-          |> Enum.map(fn n ->
-            sub = String.slice(phone, 0..n)
-            {sub |> String.to_atom(), String.replace(phone, sub, "") |> String.to_atom()}
-          end)
-          |> Enum.map(fn {w, x} ->
-            {Mnemonix.get(store, w), Mnemonix.get(store, x)}
-          end)
-        # |> Enum.map(fn
-        #   {k, v} ->
-        #     if(k == nil || v == nil) do
-        #       break
-        #     end
-
-        #     if(is_binary(k) && is_binary(v)) do
-        #       [k] ++ [v]
-        #       break
-        #     end
-
-        #     if(is_binary(k) && is_list(v)) do
-        #       Enum.reduce(v, [], fn x, acc -> [k, x | acc] end)
-        #     end
-
-        #     if(is_list(k) && is_binary(v)) do
-        #       Enum.reduce(k, [], fn x, acc -> [x, v | acc] end)
-        #     end
-
-        # if(is_list(k) && is_list(v)) do
-        #   Enum.reduce(k, [], fn x, acc -> Enum.map(v, fn d, x, acc -> [d, x | acc] end) end)
-        # end
-        # end)
-
-        # [h|t]= terms |> Tuple.to_list
-        # h|> Enum.map(fn {k, v} -> k end)
+        2..6
+        |> Enum.map(fn n ->
+          sub = String.slice(phone, 0..n)
+          {sub |> String.to_atom(), String.replace(phone, sub, "") |> String.to_atom()}
+        end)
+        |> Enum.map(fn {w, x} ->
+          {Mnemonix.get(store, w), Mnemonix.get(store, x)}
+        end)
+        |> Enum.map(fn
+          {k, v} ->
+            if(k != nil && v != nil) do
+              if(is_binary(k) && is_binary(v)) do
+                [k] ++ [v]
+              else
+                if(is_list(k) || is_list(v)) do
+                  if(is_list(k) && is_list(v)) do
+                    Enum.reduce(k, [], fn k, acc ->
+                      acc ++ Enum.map(v, fn x -> [k] ++ [x] end)
+                    end)
+                  else
+                    if(is_list(k)) do
+                      Enum.reduce(k, [], fn k, acc -> acc ++ [[k] ++ [v]] end)
+                    else
+                      Enum.reduce(v, [], fn v, acc -> acc ++ [[k] ++ [v]] end)
+                    end
+                  end
+                end
+              end
+            end
+        end)
+        |> Enum.filter(&(!is_nil(&1)))
+        |> Enum.to_list()
+        |> List.flatten()
+        |> Enum.chunk_every(2)
       }
     end
   end
