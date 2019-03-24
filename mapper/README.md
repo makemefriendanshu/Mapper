673f7a5ec961a878b699587459e4c1bd37cf694e
Anshuman <makemefriend.anshu@gmail.com>
2019-03-24 09:13:04 +0530

    added test cases

--- mapper/README.md
+++ mapper/README.md
@@ -3,8 +3,10 @@
 ## Usage
 
 ```elixir
+git clone https://github.com/makemefriendanshu/Mapper.git
+cd Mapper/mapper/
+mix deps.get
 iex -S mix
-Mapper.prepare_dictionary()
 
 Mapper.lookup(2282668687)
 [["act", "amounts"], ["act", "contour"], ["acta", "mounts"], ["bat", "amounts"], ["bat", "contour"], ["cat", "contour"], "catamounts"]
@@ -12,4 +14,14 @@ Mapper.lookup(2282668687)
 Mapper.lookup(6686787825)
 [["motor", "usual"], ["noun", "struck"], ["nouns", "truck"], ["nouns", "usual"], ["onto", "struck"], "motortruck"]
 
+```
+
+## Test
+
+```elixir
+mix test --trace
+
+MapperTest
+  * test lookup for 2282668687 (2179.1ms)
+  * test lookup for 6686787825 (2029.9ms)
 ```
--- mapper/test/mapper_test.exs
+++ mapper/test/mapper_test.exs
@@ -2,7 +2,32 @@ defmodule MapperTest do
   use ExUnit.Case
   doctest Mapper
 
-  test "greets the world" do
-    assert Mapper.hello() == :world
+  test "lookup for 2282668687" do
+    out = [
+      ["act", "amounts"],
+      ["act", "contour"],
+      ["acta", "mounts"],
+      ["bat", "amounts"],
+      ["bat", "contour"],
+      ["cat", "contour"],
+      "catamounts"
+    ]
+
+    assert Mapper.lookup(2_282_668_687) |> length() == out |> length()
+    assert Mapper.lookup(2_282_668_687) |> Enum.sort() == out |> Enum.sort()
+  end
+
+  test "lookup for 6686787825" do
+    out = [
+      ["motor", "usual"],
+      ["noun", "struck"],
+      ["nouns", "truck"],
+      ["nouns", "usual"],
+      ["onto", "struck"],
+      "motortruck"
+    ]
+
+    assert Mapper.lookup(6_686_787_825) |> length() == out |> length()
+    assert Mapper.lookup(6_686_787_825) |> Enum.sort() == out |> Enum.sort()
   end
 end
